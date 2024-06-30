218daba6d06ec902433ca1e1d7c3dfc0ff40afbb
zoras078 <zorasniraula@gmail.com>
2024-06-30 11:08:46 -0400

    simple program to add two numbers in java

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Two numbers to be added
+        int num1 = 5;
+        int num2 = 10;
+
+        // Sum of num1 and num2
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
