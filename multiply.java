b6323176e57a1ac61e8af803f3a9f25d21e4ba7f
zoras078 <zorasniraula@gmail.com>
2024-06-30 11:23:32 -0400

    multiply two numbers in java

---
+++ multiply.java
@@ -0,0 +1,14 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        // Two numbers to be multiplied
+        int num1 = 5;
+        int num2 = 10;
+
+        // Product of num1 and num2
+        int product = num1 * num2;
+
+        // Print the result
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
+
